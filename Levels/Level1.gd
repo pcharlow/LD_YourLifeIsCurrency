cb5a4a88ad7f716e0f3db31186f519d612baa6c6
pcharlow <47442624+pcharlow@users.noreply.github.com>
2019-04-28 03:25:38 -0400

    Added Art Assets

--- Levels/Level1.gd
+++ Levels/Level1.gd
@@ -4,7 +4,6 @@ extends Node2D
 # var a = 2
 # var b = "text"
 var labelSwap = false
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
